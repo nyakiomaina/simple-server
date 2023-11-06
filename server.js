1852700d412a18d7f43b1134c9847d6a14a0d524
nyakiomaina <carolinenyakio21@gmail.com>
2023-11-06 14:25:51 +0300

    fixes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = 9500;
@@ -11,7 +10,20 @@ if (!fs.existsSync(cidsDirectory)) {
   fs.mkdirSync(cidsDirectory, { recursive: true });
 }
 
-app.use(bodyParser.raw({ type: '*/*' }));
+app.use((req, res, next) => {
+  if (req.is('application/octet-stream')) {
+    let data = [];
+    req.on('data', chunk => {
+      data.push(chunk);
+    });
+    req.on('end', () => {
+      req.body = Buffer.concat(data);
+      next();
+    });
+  } else {
+    next();
+  }
+});
 
 app.get('/get/:cid', (req, res) => {
   const cid = req.params.cid;
@@ -26,21 +38,26 @@ app.get('/get/:cid', (req, res) => {
 });
 
 app.put('/put/:cid', (req, res) => {
-  const cid = req.params.cid;
-  const filePath = path.join(cidsDirectory, cid);
-  console.log(`Received PUT request for CID: ${cid}`);
-
-  fs.writeFile(filePath, req.body, (err) => {
-    if (err) {
-      console.error(`Error writing file for CID: ${cid}`, err);
-      res.status(500).send('Error writing file');
-    } else {
-      console.log(`File stored successfully for CID: ${cid}`);
-      res.status(200).send('File stored successfully');
+    const cid = req.params.cid;
+    const filePath = path.join(cidsDirectory, cid);
+    console.log(`Received PUT request for CID: ${cid} with Content-Type: ${req.headers['content-type']}`);
+  
+    if (!(req.body instanceof Buffer)) {
+      console.error(`Expected request body to be a buffer, but got: ${typeof req.body}`);
+      return res.status(400).send('Bad request body');
     }
+  
+    fs.writeFile(filePath, req.body, (err) => {
+      if (err) {
+        console.error(`Error writing file for CID: ${cid}`, err);
+        res.status(500).send('Error writing file');
+      } else {
+        console.log(`File stored successfully for CID: ${cid}`);
+        res.status(200).send('File stored successfully');
+      }
+    });
   });
-});
-
+  
 app.listen(port, '127.0.0.1', () => {
   console.log(`Server listening at http://127.0.0.1:${port}`);
 });
